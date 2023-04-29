bd2524b2c8cb6b3fc25a592986a440229a204fad
Anuj-negi2207 <pienegi22@gmail.com>
2023-04-29 23:36:52 +0530

    Codeforces Round 869 (Div. 2)

---
+++ A_A_characteristic.py
@@ -0,0 +1,88 @@
+""" THIS TEMPLATE belongs to anuj_negi || Updated - 5th Feb """
+
+""" Use sortedcontainers, Multiset when available """
+""" 'bisect' for binary search and insertion """
+import io,os
+from bisect import *
+from heapq import *
+from math import *
+from collections import defaultdict as ddc, Counter, deque
+from functools import *
+from itertools import *
+from sys import setrecursionlimit
+#from functools import cache, lru_cache
+#from typing import Generic, Iterable, Iterator, TypeVar, Union, List
+#T = TypeVar('T')
+
+
+into = io.BytesIO(os.read(0,os.fstat(0).st_size)).readline
+def input(): return into().decode().strip()
+
+def intin(): return int(input())
+def mapin(): return list(map(int, input().split()))
+
+#----------------------------------------------
+
+INF = 1<<64             #Or use 10**20
+mod = 1_000_000_007     #Or 998_244_353
+yes = "YES"
+no = "NO"
+even = "EVEN"
+odd = "ODD"
+alice = "ALICE"
+bob = "BOB"
+
+
+#----------------------------------------------
+
+"""
+SOME FACTS THAT CAN BE USED LATER - 
+
+->  if n is prime, then -> (a**(n-1)) % n = 1
+
+->  When cases like DEFICIENT CITY to SURPLUS CITY, work on BFS using SURPLUS city and check for DEFICIENT city
+
+->  PALINDROMEs and str-len FACTORS have some relations. if P = XQ where P, X, Q are palindromes, then X have the possible length in Factors of 'n'
+
+->  When all substrings are said to be checked, we can go for 2 loops (With 100 limit of inner loop, minimize the memory)
+
+->  Bezout's Lemma states that if x and y are nonzero integers and g = gcd(x,y), 
+    then there exist integers a and b such that ax+by=g. 
+    In other words, there exists a linear combination of x and y equal to g. 
+"""
+
+#----------------------------------------------
+All = ddc(int)
+for j in range(1, 101):
+    All[j] = j*(j-1)//2
+
+def process(arr):
+    n,k = arr
+    k<<=1
+
+    for i in range(n):
+        one, two = i, n-i
+        #   print(i, one*(one-1), two*(two-1))
+        if one*(one-1) + two*(two-1)==k:
+            print(yes)
+            ans = [1]*one + [-1]*two
+            print(*ans)
+            return
+    
+    print(no)   
+        
+        
+def main():
+    #T-testcases
+
+    for _ in range(intin()):
+        arr = mapin()
+        #arr = input()
+        process(arr)
+        #print("Case #{0}: {1}".format(_+1, ans))
+    
+
+#----------------------------------------------
+
+if __name__ == "__main__":
+    main()
